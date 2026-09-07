13dcc4e1f414e16bfc97b051047753d2275429fa
Dmytro <@email.com>
2026-09-07 16:37:52 +0300

    ONad typo fixed

--- task.sql
+++ task.sql
@@ -4,4 +4,4 @@ CREATE INDEX Email ON Customers(Email);
 CREATE INDEX Name ON Products(Name);
 CREATE INDEX CustomerID ON Orders(CustomerID);
 CREATE INDEX OrderID ON OrderItems(OrderID);
-CREATE INDEX ProductID ONad OrderItems(ProductID);
+CREATE INDEX ProductID ON OrderItems(ProductID);
